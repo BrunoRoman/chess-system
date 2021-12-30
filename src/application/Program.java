621932f44689f8012f080bcd9927f2d3084444d1
Bruno Roman <Bruno Roman>
2021-12-30 04:32:08 -0800

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import bordgame.Board;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Board board = new Board(8,8);
+
+	}
+
+}
